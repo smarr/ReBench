32af3c256e25562fa12ea87cfe222dcb82b9b5c7
Stefan Marr <git@stefan-marr.de>
2013-03-16 19:29:37 +0100

    Fix path in coverage.sh
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nosetests-2.7 --with-coverage --cover-inclusive --cover-branches --cover-html --cover-html-dir=cover-results --cover-package=rebench --cover-erase --cover-tests
+nosetests-2.7 --with-coverage --cover-inclusive --cover-branches --cover-html --cover-html-dir=../cover-results --cover-package=rebench --cover-erase --cover-tests
