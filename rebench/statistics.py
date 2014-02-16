c7484d9a80d7bf6e9119313ab5827337fe49f634
Stefan Marr <git@stefan-marr.de>
2014-02-16 12:21:24 +0100

    Rename and adopt proper Python naming conventions
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/statistics.py
+++ rebench/statistics.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2011 Stefan Marr <http://www.stefan-marr.de/>
+# Copyright (c) 2009-2014 Stefan Marr <http://www.stefan-marr.de/>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
@@ -47,48 +47,54 @@ class StatisticProperties:
     from a set of data points
     """
     
-    def __init__(self, dataSamples, confidence_level):
-        self._dataSamples = dataSamples
-        self._confidenceLevel = confidence_level
-        
-        if self._dataSamples:
-            self._calcBasicStatistics()
-            self._calcConfidence(confidence_level)
-            self.failedRun = False
+    def __init__(self, data_samples, confidence_level):
+        self._data_samples     = data_samples
+        self._confidence_level = confidence_level
+
+        self.mean                   = 0
+        self.geom_mean              = 0
+        self.median                 = 0
+        self.std_dev                = 0
+        self.num_samples            = 0
+        self.min                    = 0
+        self.max                    = 0
+        self.conf_interval_low      = 0
+        self.conf_interval_high     = 0
+        self.conf_interval_size_abs = 0
+        self.conf_interval_size     = 0
+
+        if self._data_samples:
+            self.num_samples = len(self._data_samples)
+            self._calc_basic_statistics()
+            self._calc_confidence(confidence_level)
         else:
-            self.failedRun = True
-            self.numSamples = 0
-            
-        
-    def _calcBasicStatistics(self):
+            self.num_samples = 0
+
+    def _calc_basic_statistics(self):
         """This function determines the mean and the standard deviation
            of the data sample.
            Furthermore, several other simple properties are determined.
         """
-        self.mean       = stats.mean(self._dataSamples)
-        self.geom_mean  = stats.geomean(self._dataSamples)
-        self.median     = stats.median(self._dataSamples)
-        self.stdDev     = stats.stddev(self._dataSamples)
-        self.numSamples = len(self._dataSamples)
+        self.mean        = stats.mean(self._data_samples)
+        self.geom_mean   = stats.geomean(self._data_samples)
+        self.median      = stats.median(self._data_samples)
+        self.std_dev     = stats.stddev(self._data_samples)
 
-        self.min = min(self._dataSamples)
-        self.max = max(self._dataSamples)
+        self.min = min(self._data_samples)
+        self.max = max(self._data_samples)
         
     def as_tuple(self):
-        if self.failedRun:
-            return (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-        
         return (self.mean,
                 self.geom_mean,
                 self.median,
-                self.stdDev,
-                self.numSamples,
+                self.std_dev,
+                self.num_samples,
                 self.min,
                 self.max,
-                self.confIntervalLow,
-                self.confIntervalHigh,
-                self.confIntervalSizeAbs,
-                self.confIntervalSize)
+                self.conf_interval_low,
+                self.conf_interval_high,
+                self.conf_interval_size_abs,
+                self.conf_interval_size)
     
     @classmethod
     def tuple_mapping(cls):
@@ -97,7 +103,7 @@ class StatisticProperties:
                 'Conf. Interval High', 'Conf. Interval Size Abs.',
                 'Conf.IntervalSize/Mean')
 
-    def _calcConfidence(self, confidence_level):
+    def _calc_confidence(self, confidence_level):
         """
         Depending on the number of samples, different distributions
         are more optimal.
@@ -106,23 +112,21 @@ class StatisticProperties:
         javastats used students,
         i.e., t distribution for fewer values (<=30 values)
         """
-        if self.numSamples > 30:
+        if self.num_samples > 30:
             distribution = stats.norm_ppf((1 + confidence_level)/2.0)
         else:
-            df   = self.numSamples - 1
+            df   = self.num_samples - 1
             distribution = stats.t_ppf((1 + confidence_level)/2.0, df)
             
-        self._confidenceForSamples(distribution)
+        self._confidence_for_samples(distribution)
             
-    def _confidenceForSamples(self, distribution):
+    def _confidence_for_samples(self, distribution):
         """This function determines the confidence interval for a given 
            set of samples, as well as  and the size of the confidence 
            interval and its percentage of the mean.
         """
+        self.conf_interval_low  = self.mean - (distribution * self.std_dev / math.sqrt(self.num_samples))
+        self.conf_interval_high = self.mean + (distribution * self.std_dev / math.sqrt(self.num_samples))
         
-        self.confIntervalLow  = self.mean - (distribution * self.stdDev / math.sqrt(self.numSamples))
-        self.confIntervalHigh = self.mean + (distribution * self.stdDev / math.sqrt(self.numSamples))
-        
-        self.confIntervalSizeAbs = self.confIntervalHigh - self.confIntervalLow
-        self.confIntervalSize = self.confIntervalSizeAbs / self.mean  
-
+        self.conf_interval_size_abs = self.conf_interval_high - self.conf_interval_low
+        self.conf_interval_size     = self.conf_interval_size_abs / self.mean
