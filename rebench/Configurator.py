5977f3bc8fefc375358b8b50e5129b32c1cf2188
Stefan Marr <git@stefan-marr.de>
2014-02-16 19:05:23 +0100

    Cleanup of Configurator, don't use magic properties anymore
    - adopt PEP 8 style
    - removed magic property lookup, let's be more explicit
    - made methods return values to initialize fields directly in constructor
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/Configurator.py
+++ rebench/Configurator.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2011 Stefan Marr <http://www.stefan-marr.de/>
+# Copyright (c) 2009-2014 Stefan Marr <http://www.stefan-marr.de/>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
@@ -27,6 +27,7 @@ from .model.experiment  import Experiment
 
 from copy import deepcopy
 
+
 def dict_merge_recursively(a, b):
     """Merges two dicts recursively.
        Both initial dicts remain unchanged."""
@@ -46,36 +47,37 @@ def dict_merge_recursively(a, b):
 
 
 class Configurator:
-    
-    
-    def __init__(self, fileName, cliOptions, expName = None):
-        self._load_config(fileName)
-        self._process_cli_options(cliOptions)
-        self._exp_name  = expName
+
+    def __init__(self, file_name, cli_options, exp_name = None):
+        self._raw_config = self._load_config(file_name)
+        self._options    = self._process_cli_options(cli_options)
+        self._exp_name   = exp_name
         
-        self.runs       = RunsConfig(     **self._rawConfig.get(      'runs', {}))
-        self.quick_runs = QuickRunsConfig(**self._rawConfig.get('quick_runs', {}))
+        self.runs        = RunsConfig(     **self._raw_config.get(      'runs', {}))
+        self.quick_runs  = QuickRunsConfig(**self._raw_config.get('quick_runs', {}))
         
         self._experiments = self._compile_experiments()
         
-        self.visualization = self._rawConfig['experiments'][self.experiment_name()].get('visualization', None)
-    def __getattr__(self, name):
-        return self._rawConfig.get(name, None)
-    
-    def _load_config(self, file_name):
+        # TODO: does visualization work?
+        # self.visualization = self._raw_config['experiments'][self.experiment_name()].get('visualization', None)
+            
+    @staticmethod
+    def _load_config(file_name):
         try:
             f = file(file_name, 'r')
-            self._rawConfig = yaml.load(f)
+            return yaml.load(f)
         except IOError:
-            logging.error("There was an error opening the config file (%s)."%(file_name))
+            logging.error("An error occurred on opening the config file (%s)."
+                          % file_name)
             logging.error(traceback.format_exc(0))
             sys.exit(-1)
         except yaml.YAMLError:
-            logging.error("Failed parsing the config file (%s)."%(file_name))
+            logging.error("Failed parsing the config file (%s)." % file_name)
             logging.error(traceback.format_exc(0))
             sys.exit(-1)
-            
-    def _process_cli_options(self, options):
+
+    @staticmethod
+    def _process_cli_options(options):
         if options is None:
             return
         
@@ -87,23 +89,26 @@ class Configurator:
             logging.basicConfig(level=logging.ERROR)
             logging.getLogger().setLevel(logging.ERROR)
                     
-        self.options = options
-    
+        return options
+
+    @property
+    def options(self):
+        return self._options
+
     @property
     def use_nice(self):
         return self.options is not None and self.options.use_nice
         
     def experiment_name(self):
-        return self._exp_name or self.standard_experiment
+        return self._exp_name or self._raw_config['standard_experiment']
     
     def data_file_name(self):
-        """@TODO: might add a command-line option 'ff' is just a placeholder here..."""
-        
-        data_file = self._rawConfig['experiments'][self.experiment_name()].get('data_file', None)
+        data_file = self._raw_config['experiments'][self.experiment_name()].get(
+            'data_file', None)
         if data_file:
             return data_file
         
-        return self.standard_data_file
+        return self._raw_config['standard_data_file']
     
     def get_experiments(self):
         """The configuration has been compiled before it is handed out
@@ -122,38 +127,30 @@ class Configurator:
             runs |= exp.get_runs()
         return runs
     
-    
-    def _valueOrListAllwaysAsList(self, value):
-        if type(value) is list:
-            return value
-        elif value is None:
-            return []
-        else:
-            return [value]
-    
     def _compile_experiments(self):
         if not self.experiment_name():
             raise ValueError("No experiment chosen.")
         
-        confDefs = {}
+        conf_defs = {}
         
         if self.experiment_name() == "all":
-            for exp_name in self._rawConfig['experiments']:
-                confDefs[exp_name] = self._compile_experiment(exp_name)
+            for exp_name in self._raw_config['experiments']:
+                conf_defs[exp_name] = self._compile_experiment(exp_name)
         else:
-            if self.experiment_name() not in self._rawConfig['experiments']:
-                raise ValueError("Requested experiment '%s' not available." % self.experiment_name())
-            confDefs[self.experiment_name()] = self._compile_experiment(self.experiment_name())
+            if self.experiment_name() not in self._raw_config['experiments']:
+                raise ValueError("Requested experiment '%s' not available." %
+                                 self.experiment_name())
+            conf_defs[self.experiment_name()] = self._compile_experiment(
+                                                        self.experiment_name())
         
-        return confDefs
+        return conf_defs
     
     def _compile_experiment(self, exp_name):
-        expDef = self._rawConfig['experiments'][exp_name]
-        run_cfg = self.quick_runs if (self.options and self.options.quick) else self.runs
+        exp_def = self._raw_config['experiments'][exp_name]
+        run_cfg = (self.quick_runs if (self.options and self.options.quick)
+                                   else self.runs)
         
-        return Experiment(exp_name, expDef, run_cfg,
-                         self._rawConfig['virtual_machines'],
-                         self._rawConfig['benchmark_suites'],
-                         self._rawConfig['reporting'])
-    
-    
+        return Experiment(exp_name, exp_def, run_cfg,
+                          self._raw_config['virtual_machines'],
+                          self._raw_config['benchmark_suites'],
+                          self._raw_config['reporting'])
