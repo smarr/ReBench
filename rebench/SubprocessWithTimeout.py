642f1b27a137e6b709c242513d9f982a4f8bb27a
Stefan Marr <git@stefan-marr.de>
2014-02-16 20:40:52 +0100

    Cleanup Python style
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/SubprocessWithTimeout.py
+++ rebench/SubprocessWithTimeout.py
@@ -1,17 +1,21 @@
-## Imported from http://stackoverflow.com/questions/1191374/subprocess-with-timeout
+## Imported from:
+##   http://stackoverflow.com/questions/1191374/subprocess-with-timeout
 ## Includes small modifications STEFAN 2011-01-26
 
 from os import kill
 from signal import alarm, signal, SIGALRM, SIGKILL
 from subprocess import PIPE, Popen
 
-def run(args, cwd = None, shell = False, kill_tree = True, timeout = -1, stdout = PIPE, stderr = PIPE):
-    '''
+
+def run(args, cwd = None, shell = False, kill_tree = True, timeout = -1,
+        stdout = PIPE, stderr = PIPE):
+    """
     Run a command with a timeout after which it will be forcibly
     killed.
-    '''
+    """
     class Alarm(Exception):
         pass
+
     def alarm_handler(signum, frame):
         raise Alarm
     
@@ -32,12 +36,9 @@ def run(args, cwd = None, shell = False, kill_tree = True, timeout = -1, stdout 
         return -9, '', ''
     return p.returncode, stdout, stderr
 
+
 def get_process_children(pid):
     p = Popen('ps --no-headers -o pid --ppid %d' % pid, shell = True,
               stdout = PIPE, stderr = PIPE)
     stdout, _stderr = p.communicate()
     return [int(p) for p in stdout.split()]
-
-#if __name__ == '__main__':
-#    print run('find /', shell = True, timeout = 3)
-#    print run('find', shell = True)
