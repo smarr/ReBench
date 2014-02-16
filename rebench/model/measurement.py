1e30c378347a474ef006eedbb53a5fbfe418fd10
Stefan Marr <git@stefan-marr.de>
2014-02-16 19:20:07 +0100

    Completed Measurement class, added persistency support
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/model/measurement.py
+++ rebench/model/measurement.py
@@ -1,15 +1,18 @@
-class Measurement:
-    def __init__(self, value, bench_name = None, criterion = 'total', unit = None):
-        self._bench_name = bench_name
-        self._criterion  = criterion
-        self._value      = value
+from datetime import datetime
+
+from rebench.model.run_id import RunId
+
+
+class Measurement(object):
+    def __init__(self, value, unit, run_id, criterion = 'total', timestamp = None):
+        self._run_id    = run_id
+        self._criterion = criterion
+        self._value     = value
+        self._unit      = unit
+        self._timestamp = timestamp or datetime.now()
         
     def is_total(self):
         return self._criterion == 'total'
-    
-    @property
-    def bench_name(self):
-        return self._bench_name
     
     @property
     def criterion(self):
@@ -22,3 +25,30 @@ class Measurement:
     @property
     def unit(self):
         return self._unit
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @property
+    def run_id(self):
+        return self._run_id
+
+    TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+    def as_str_list(self):
+        return ["[" + self._timestamp.strftime(self.TIME_FORMAT) + "]",
+                "%f" % self._value,
+                self._unit,
+                self._criterion] + self._run_id.as_str_list()
+
+    @classmethod
+    def from_str_list(cls, str_list):
+
+        timestamp = datetime.strptime(str_list[0][1:-1], cls.TIME_FORMAT)
+        value     = float(str_list[1])
+        unit      = str_list[2]
+        criterion = str_list[3]
+        run_id    = RunId.from_str_list(str_list[4:])
+
+        return Measurement(value, unit, run_id, criterion, timestamp)
