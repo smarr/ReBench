5f9ecc584c3480abf9408fd5257e6d801c2fa7ae
Stefan Marr <git@stefan-marr.de>
2014-02-18 23:59:23 +0100

    Added test for issue #4
    Closes #4 since it shows that the bug is gone.
    Was solved by introducing the RunId equality based on the command line.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ rebench/tests/bugs/issue_4_run_equality_and_params_test.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2009-2014 Stefan Marr <http://www.stefan-marr.de/>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import os
+import unittest
+from rebench.model.benchmark_config import BenchmarkConfig
+from rebench.model.benchmark_suite  import BenchmarkSuite
+from rebench.model.run_id           import RunId
+from rebench.model.virtual_machine  import VirtualMachine
+
+
+class Issue4RunEquality(unittest.TestCase):
+
+    def setUp(self):
+        self._path = os.path.dirname(os.path.realpath(__file__))
+
+    def _create_template_run_id(self):
+        vm        = VirtualMachine("MyVM", None, {'path':   'foo_bar_path',
+                                                  'binary': 'foo_bar_bin'},
+                                   None, [1], None)
+        suite     = BenchmarkSuite("MySuite", vm, {
+            'benchmarks': [], 'performance_reader': '',
+            'command': '%(benchmark)s %(cores)s %(input)s'})
+        bench_cfg = BenchmarkConfig("TestBench", None, suite, vm, '3')
+        return RunId(bench_cfg, 1, 2, None)
+
+    def _create_hardcoded_run_id(self):
+        vm        = VirtualMachine("MyVM", None, {'path':   'foo_bar_path',
+                                                  'binary': 'foo_bar_bin'},
+                                   None, [1], None)
+        suite     = BenchmarkSuite("MySuite", vm, {
+            'benchmarks': [], 'performance_reader': '',
+            'command': '%(benchmark)s %(cores)s 2 3'})
+        bench_cfg = BenchmarkConfig("TestBench", None, suite, vm)
+        return RunId(bench_cfg, 1, None, None)
+
+    def test_hardcoded_equals_template_constructed(self):
+        hard_coded = self._create_hardcoded_run_id()
+        template   = self._create_template_run_id()
+
+        self.assertEquals(hard_coded.cmdline(), template.cmdline())
+        self.assertEquals(hard_coded, template)
+        self.assertTrue(hard_coded == template)
+        self.assertFalse(hard_coded is template)
+
