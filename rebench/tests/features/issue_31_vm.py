3e0805affc0f244c68082705e441097f492082bc
Tobias Pape <tobias@netshed.de>
2014-05-19 22:07:56 +0200

    Add a Multivariate Performance reader

---
+++ rebench/tests/features/issue_31_vm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+## simple script emulating a VM generating benchmark results
+import sys
+
+print sys.argv
+
+print "Harness Name: ", sys.argv[1]
+print "Bench Name:",    sys.argv[2]
+print "Input Size: ",   sys.argv[3]
+
+input_size = int(sys.argv[3])
+
+for i in range(0, input_size):
+    print "%d:RESULT-bar:ms:   %d.%d" % (i, i, i)
+    print "%d:RESULT-total:    %d.%d" % (i, i, i)
+    print "%d:RESULT-baz:kbyte:   %d" % (i, i)
+    print "%d:RESULT-foo:kerf: %d.%d" % (i, i, i)
