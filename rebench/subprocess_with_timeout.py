6998fa468907d7f25309c1f925b305e216e52559
Stefan Marr <git@stefan-marr.de>
2014-10-01 15:47:37 +0200

    Subprocess timeout rewritten to use threads
    This change is necessary to enable threaded execution, because signals can only be handled by the main thread.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/subprocess_with_timeout.py
+++ rebench/subprocess_with_timeout.py
@@ -1,10 +1,31 @@
-## Imported from:
-##   http://stackoverflow.com/questions/1191374/subprocess-with-timeout
-## Includes small modifications STEFAN 2011-01-26
-
-from os import kill
-from signal import alarm, signal, SIGALRM, SIGKILL
+from os         import kill
+from signal     import SIGKILL
 from subprocess import PIPE, Popen
+from threading  import Thread
+
+
+class SubprocessThread(Thread):
+
+    def __init__(self, binary_name, args, shell, cwd, stdout, stderr):
+        Thread.__init__(self, name = "Subprocess %s" % binary_name)
+        self._args   = args
+        self._shell  = shell
+        self._cwd    = cwd
+        self._stdout = stdout
+        self._stderr = stderr
+
+        self.stdout_result = None
+        self.stderr_result = None
+        self.returncode    = None
+        self.pid           = None
+
+    def run(self):
+        p = Popen(self._args, shell=self._shell, cwd=self._cwd,
+                  stdout=self._stdout, stderr=self._stderr)
+        self.pid = p.pid
+
+        self.stdout_result, self.stderr_result = p.communicate()
+        self.returncode = p.returncode
 
 
 def run(args, cwd = None, shell = False, kill_tree = True, timeout = -1,
@@ -13,23 +34,24 @@ def run(args, cwd = None, shell = False, kill_tree = True, timeout = -1,
     Run a command with a timeout after which it will be forcibly
     killed.
     """
-    class Alarm(Exception):
-        pass
+    binary_name = args.split(' ')[0]
 
-    def alarm_handler(signum, frame):
-        raise Alarm
-    
-    p = Popen(args, shell=shell, cwd=cwd, stdout=stdout, stderr=stderr)
-    if timeout != -1:
-        signal(SIGALRM, alarm_handler)
-        alarm(timeout)
-    try:
-        stdout, stderr = p.communicate()
-        if timeout != -1:
-            alarm(0)
-    except Alarm:
-        return kill_process(p.pid, kill_tree)
-    return p.returncode, stdout, stderr
+    thread = SubprocessThread(binary_name, args, shell, cwd, stdout, stderr)
+    thread.start()
+
+    if timeout == -1:
+        print "No timeout"
+        thread.join()
+    else:
+        print "Timeout: %d" % timeout
+        thread.join(timeout)
+
+    if timeout != -1 and thread.is_alive():
+        assert thread.pid is not None
+        return kill_process(thread.pid, kill_tree)
+
+    return thread.returncode, thread.stdout_result, thread.stderr_result
+
 
 def kill_process(pid, recursively):
     pids = [pid]
