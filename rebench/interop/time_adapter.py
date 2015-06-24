4d915004bbdfdb57074bc23d4a223806059da90a
Stefan Marr <git@stefan-marr.de>
2015-06-24 15:16:07 +0200

    Added support for reading out Max RSS on Linux
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/interop/time_adapter.py
+++ rebench/interop/time_adapter.py
@@ -18,8 +18,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 import re
-from .adapter         import GaugeAdapter, OutputNotParseable,\
-    ResultsIndicatedAsInvalid
+import subprocess
+from .adapter            import GaugeAdapter, OutputNotParseable
 from ..model.data_point  import DataPoint
 from ..model.measurement import Measurement
 
@@ -32,8 +32,27 @@ class TimeAdapter(GaugeAdapter):
     re_time = re.compile(r"^(\w+)\s*(\d+)m(\d+\.\d+)s")
     re_time2 = re.compile(r"^(\w+)(\s*)(\d+\.\d+)")
 
+    # To be sure about how to parse the output, give custom format
+    # This avoids issues with perhaps none-standard /bin/usr/time
+    time_format = 'max rss (kb): %M\nwall-time (secounds): %e\n'
+    re_formatted_time = re.compile(r"^wall-time \(secounds\): (\d+\.\d+)")
+    re_formatted_rss  = re.compile(r"^max rss \(kb\): (\d+)")
+
+    def __init__(self, include_faulty):
+        GaugeAdapter.__init__(self, include_faulty)
+        self._use_formatted_time = False
+
     def acquire_command(self, command):
-        return "/usr/bin/time -p %s" % command
+        formatted_output = subprocess.call(
+            ['/usr/bin/time', '-f', TimeAdapter.time_format, 'sleep', '1'],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if formatted_output == 0:
+            self._use_formatted_time = True
+            return "/usr/bin/time -t %s %s" % (TimeAdapter.time_format, command)
+        else:
+            # use standard, but without info on memory
+            # TODO: add support for reading out memory info on OS X
+            return "/usr/bin/time -p %s" % command
 
     def parse_data(self, data, run_id):
         data_points = []
@@ -43,15 +62,28 @@ class TimeAdapter(GaugeAdapter):
             if self.check_for_error(line):
                 return None
 
-            m1 = self.re_time.match(line)
-            m2 = self.re_time2.match(line)
-            if m1 or m2:
-                m = m1 or m2
-                criterion = 'total' if m.group(1) == 'real' else m.group(1)
-                time = (float(m.group(2).strip() or 0) * 60 +
-                        float(m.group(3))) * 1000
-                measure = Measurement(time, 'ms', run_id, criterion)
-                current.add_measurement(measure)
+            if self._use_formatted_time:
+                m1 = self.re_formatted_rss.match(line)
+                m2 = self.re_formatted_time.match(line)
+                if m1:
+                    mem_kb = m1.group(1)
+                    measure = Measurement(mem_kb, 'kb', run_id, 'MaxRSS')
+                    current.add_measurement(measure)
+                elif m2:
+                    time = float(m2.group(1)) * 1000
+                    measure = Measurement(time, 'ms', run_id, 'total')
+                    current.add_measurement(measure)
+                    current = DataPoint(run_id)
+            else:
+                m1 = self.re_time.match(line)
+                m2 = self.re_time2.match(line)
+                if m1 or m2:
+                    m = m1 or m2
+                    criterion = 'total' if m.group(1) == 'real' else m.group(1)
+                    time = (float(m.group(2).strip() or 0) * 60 +
+                            float(m.group(3))) * 1000
+                    measure = Measurement(time, 'ms', run_id, criterion)
+                    current.add_measurement(measure)
 
                 if measure.is_total():
                     data_points.append(current)
