bc2ac9a0fc3132bcea8284a98231c848c1940608
Stefan Marr <git@stefan-marr.de>
2019-04-17 15:35:13 +0100

    Added more tests for different scenarios
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/tests/bugs/issue_112_invocations_setting_ignored_test.py
+++ rebench/tests/bugs/issue_112_invocations_setting_ignored_test.py
@@ -11,11 +11,11 @@ class Issue112Test(ReBenchTestCase):
         super(Issue112Test, self).setUp()
         self._set_path(__file__)
 
-    def test_iteration_invocation_semantics(self):
+    def test_invocation_setting_on_experiment(self):
         # Executes first time
         ds = DataStore(self._ui)
         cnf = Configurator(load_config(self._path + '/issue_112.conf'),
-                           ds, self._ui, data_file=self._tmp_file)
+                           ds, self._ui, exp_name='ExpSetting', data_file=self._tmp_file)
         ds.load_data(None, False)
 
         # Has not executed yet, check that there is simply
@@ -25,5 +25,48 @@ class Issue112Test(ReBenchTestCase):
         ex.execute()
 
         self._assert_runs(cnf, 1, 10, 10)
-        self.assertTrue(False)
 
+    def test_invocation_setting_on_experiment_execution_detail(self):
+        # Executes first time
+        ds = DataStore(self._ui)
+        cnf = Configurator(load_config(self._path + '/issue_112.conf'),
+                           ds, self._ui, exp_name='ExecSetting', data_file=self._tmp_file)
+        ds.load_data(None, False)
+
+        # Has not executed yet, check that there is simply
+        self._assert_runs(cnf, 1, 0, 0)
+
+        ex = Executor(cnf.get_runs(), False, False, self._ui)
+        ex.execute()
+
+        self._assert_runs(cnf, 1, 7, 7)
+
+    def test_invocation_setting_for_global_run_details(self):
+        # Executes first time
+        ds = DataStore(self._ui)
+        cnf = Configurator(load_config(self._path + '/issue_112.conf'),
+                           ds, self._ui, exp_name='GlobalSetting', data_file=self._tmp_file)
+        ds.load_data(None, False)
+
+        # Has not executed yet, check that there is simply
+        self._assert_runs(cnf, 1, 0, 0)
+
+        ex = Executor(cnf.get_runs(), False, False, self._ui)
+        ex.execute()
+
+        self._assert_runs(cnf, 1, 5, 5)
+
+    def test_invocation_setting_in_suite(self):
+        # Executes first time
+        ds = DataStore(self._ui)
+        cnf = Configurator(load_config(self._path + '/issue_112.conf'),
+                           ds, self._ui, exp_name='SuiteSetting', data_file=self._tmp_file)
+        ds.load_data(None, False)
+
+        # Has not executed yet, check that there is simply
+        self._assert_runs(cnf, 1, 0, 0)
+
+        ex = Executor(cnf.get_runs(), False, False, self._ui)
+        ex.execute()
+
+        self._assert_runs(cnf, 1, 3, 3)
