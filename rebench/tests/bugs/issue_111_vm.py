63120bb3b05d47f3ce52f8cfd0f55105189d581e
Stefan Marr <git@stefan-marr.de>
2019-04-23 22:23:47 +0100

    Added test to confirm issue
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ rebench/tests/bugs/issue_111_vm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# simple script emulating a failing benchmark
+from __future__ import print_function
+
+print("Bench: iterations=1 runtime: 1000ms")
+print("Bench: iterations=1 runtime: 1000ms")
+print("Bench: iterations=1 runtime: 10ms")
+print("Bench: iterations=1 runtime: 10ms")
+print("Bench: iterations=1 runtime: 10ms")
+print("Bench: iterations=1 runtime: 10ms")
+print("Bench: iterations=1 runtime: 10ms")
+print("Bench: iterations=1 runtime: 10ms")
+print("Bench: iterations=1 runtime: 10ms")
