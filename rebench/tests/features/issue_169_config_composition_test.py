b98c8da535ee73129fd7bab694d4aa71adf55ae2
Stefan Marr <git@stefan-marr.de>
2021-11-07 23:13:31 +0000

    Add baseline settings for priority of composition
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ rebench/tests/features/issue_169_config_composition_test.py
@@ -0,0 +1,29 @@
+from ..rebench_test_case import ReBenchTestCase
+from ...configurator import Configurator, load_config
+from ...persistence import DataStore
+
+
+class Issue169ConfigCompositionTest(ReBenchTestCase):
+
+    def setUp(self):
+        super(Issue169ConfigCompositionTest, self).setUp()
+        self._set_path(__file__)
+        self.cnf = Configurator(load_config(self._path + '/issue_169.conf'),
+                                DataStore(self.ui), self.ui, None, 'all')
+        self.runs = list(self.cnf.get_runs())
+        self.runs = sorted(self.runs, key=lambda e: e.cmdline())
+
+    def _assert(self, run, exe, bench, iterations, invocations):
+        self.assertEqual(run.benchmark.suite.executor.name, exe)
+        self.assertEqual(run.benchmark.name, bench)
+        self.assertEqual(run.iterations, iterations)
+        self.assertEqual(run.invocations, invocations)
+
+    def test_confirm_setting_priority(self):
+        self._assert(self.runs[0], "TestRunner1", "Bench1", 40, 1)
+        self._assert(self.runs[1], "TestRunner1", "Bench2", 30, 1)
+
+        self._assert(self.runs[2], "TestRunner2", "Bench1", 40, 1)
+        self._assert(self.runs[3], "TestRunner2", "Bench2", 30, 1)
+        self._assert(self.runs[4], "TestRunner2", "Bench3", 10, 3)
+        self._assert(self.runs[5], "TestRunner2", "Bench4", 10, 1)
