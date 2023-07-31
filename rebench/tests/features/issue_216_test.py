8e1d0967e20829499164747698715c274607762e
naomiGrew <97107839+naomiGrew@users.noreply.github.com>
2023-07-31 16:57:35 +0100

    Update to follow same style as issue_31

--- rebench/tests/features/issue_216_test.py
+++ rebench/tests/features/issue_216_test.py
@@ -1,36 +1,52 @@
-import os
-
+# Copyright (c) 2014 Tobias Pape
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+from ...configurator           import Configurator, load_config
+from ...executor               import Executor
+from ...persistence            import DataStore
+from ..persistence import TestPersistence
 from ..rebench_test_case import ReBenchTestCase
-from ...executor          import Executor
-from ...configurator      import Configurator, load_config
-from ...persistence       import DataStore
 
 
-class RetrieveIvocationTest(ReBenchTestCase):
+class Issue216CurrentInvocation(ReBenchTestCase):
 
     def setUp(self):
-        super(RetrieveIvocationTest, self).setUp()
-        os.chdir(self._path + '/../')
+        super(Issue216CurrentInvocation, self).setUp()
+        self._set_path(__file__)
 
-    def test_get_invocation_from_cmdline(self):
-        cnf = Configurator(load_config(os.path.join(self._path,'features', 'issue_216.conf')),
-                           DataStore(self.ui), self.ui, None,
+    def _records_data_points(self, exp_name, num_data_points):
+        cnf = Configurator(load_config(self._path + '/issue_216.conf'), DataStore(self.ui),
+                           self.ui, exp_name=exp_name,
                            data_file=self._tmp_file)
         runs = cnf.get_runs()
-        ex = Executor(runs, cnf.do_builds, self.ui)
+
+        persistence = TestPersistence()
+        persistence.use_on(runs)
+        ex = Executor(runs, False, self.ui)
         ex.execute()
-        cmdlines = [run_id.get_cmdline() for run_id in ex.runs]
-        for cmd in cmdlines:
-            sequence_numbers = [self.get_sequence_number(cmdline) for cmdline in cmd]
-            is_increasing = self.is_list_increasing(sequence_numbers)
-            self.assertTrue(is_increasing)
+        self.assertEqual(1, len(cnf.get_runs()))
+        run = next(iter(cnf.get_runs()))
+        self.assertEqual(num_data_points, run.get_number_of_data_points())
+        return persistence.get_data_points()
 
-    def is_list_increasing(self,lst):
-        return all(x < y for x, y in zip(lst, lst[1:]))
-
-    def get_sequence_number(self,cmdline):
-        last_element = cmdline.split()[-1]
-        try:
-            return int(last_element)
-        except ValueError:
-            return float('-inf')
+    def test_associates_measurements_and_data_points_correctly(self):
+        data_points = self._records_data_points('Test1', 10)
+        for point, i in zip(data_points, range(10)):  
+            for measurement in point.get_measurements():
+                self.assertEqual(i, int(measurement.value))
