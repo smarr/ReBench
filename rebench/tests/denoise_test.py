0bd3454613d38e62d26f9e933fb4a9299ddbeb6e
Stefan Marr <git@stefan-marr.de>
2025-01-05 14:25:50 +0000

    Added test to make sure setting no_turbo is piked up as used feature
    - rename max_union to give more context
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- rebench/tests/denoise_test.py
+++ rebench/tests/denoise_test.py
@@ -15,12 +15,32 @@ class DenoiseTest(ReBenchTestCase):
         self.assertEqual(result, Denoise.system_default())
         assert not result.needs_denoise()
 
-    def test_max_union(self):
+    def test_max_union_default_vs_system_default(self):
         sys_defaults = Denoise.system_default()
         defaults = Denoise.default()
 
-        union = Denoise.max_union(sys_defaults, defaults)
+        union = Denoise.max_union_to_get_used_features(sys_defaults, defaults)
         self.assertEqual(union, defaults)
 
-        union = Denoise.max_union(defaults, sys_defaults)
+        union = Denoise.max_union_to_get_used_features(defaults, sys_defaults)
         self.assertEqual(union, defaults)
+
+    def test_no_turbo_is_identified_as_used(self):
+        sys_defaults = Denoise.system_default()
+        new_settings = Denoise.system_default()
+        new_settings.no_turbo = True
+
+        union = Denoise.max_union_to_get_used_features(sys_defaults, new_settings)
+        self.assertEqual(union, new_settings)
+
+        union = Denoise.max_union_to_get_used_features(new_settings, sys_defaults)
+        self.assertEqual(union, new_settings)
+
+        new_settings.no_turbo = False
+
+        union = Denoise.max_union_to_get_used_features(sys_defaults, new_settings)
+        self.assertEqual(union, new_settings)
+
+        union = Denoise.max_union_to_get_used_features(new_settings, sys_defaults)
+        self.assertEqual(union, new_settings)
+        self.assertTrue(new_settings.requested_no_turbo)
